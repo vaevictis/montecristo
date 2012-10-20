4c8a923b2219fd41551e0e4d486b0e74fcd6c7a7
Guillaume Hammadi <ghammadi@gmail.com>
2012-10-21 12:38:20 +1300

    Initial Commit

---
+++ montecristo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  montecristo
+//
+//  Created by vaevictis on 21/10/12.
+//  Copyright (c) 2012 boost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ montecristo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  montecristo
+//
+//  Created by vaevictis on 21/10/12.
+//  Copyright (c) 2012 boost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
