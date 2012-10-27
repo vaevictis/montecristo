5fe39987397daf3d09ff84fad87061955d8bde15
Guillaume Hammadi <ghammadi@gmail.com>
2012-10-28 02:13:21 +1300

    Removed balanceController

--- montecristo/BalanceController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface BalanceController : UIViewController
-
-@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (strong, nonatomic) IBOutlet UILabel *overallExpensesField;
-@property (strong, nonatomic) IBOutlet UIView *customView;
-
-@end
